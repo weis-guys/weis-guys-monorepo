4bf9fd834575957150fea36583e5447e957d320c
Jacob Weisenburger <jacob.weisenburger@gmail.com>
2022-10-01 21:47:57 -0500

    changes

--- apps/royal-drive-dms-web-app/constants/DB.ts
+++ apps/royal-drive-dms-web-app/constants/DB.ts
@@ -1,4 +1,4 @@
+import { makeFreerstoreDB } from '@weis-guys/freerstore'
 import { FIREBASE } from '../constants/FIREBASE'
-import { makeFreerstoreDB } from '../lib/freerstore/db'
 
 export const DB = makeFreerstoreDB( FIREBASE.app )
